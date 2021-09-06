6c6f4d3855bc3ad5b8a126fbfcad0cc5ff4bcc0a
nicolas-schmitz <73204052+nicolas-schmitz@users.noreply.github.com>
2021-09-05 22:48:41 -0300

    Added docstring to insertion_sort

--- ordering/methods.py
+++ ordering/methods.py
@@ -1,4 +1,14 @@
-def insertion_sort(vector):
+def insertion_sort(vector: list):
+    """Sort the vector utilizign insertion sort. Loops through the vector and finishes each iteration sorting the vector to position j.
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+
+    Args:
+        vector (list): list with values
+
+    Returns:
+        list: list with sorted values
+    """
     for j in range(1, len(vector)):
         key = vector[j]
         i = j - 1
